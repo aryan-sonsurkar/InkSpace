82ae373e9ba24500a4d7214ccf2aa52b29fccd13
Aryan Sonsurkar <aryansonsurkar87@gmail.com>
2026-08-23 18:29:40 +0530

    Major UI of landing page is almost done

--- inkspace/app/LandingPage.jsx
+++ inkspace/app/LandingPage.jsx
@@ -1,13 +1,30 @@
 import "./LandingPage.css"
+import { NotebookPen , Star} from "lucide-react";
 
 export default function LandingPage(){
 return(
     <div>
-    <section className="Heading">
-        <h1>Welcome to InkSpace</h1>
+    <section>
+    <nav className="Navbar">
+        <h1 className="Navbar-Icon"><NotebookPen /> </h1>
+        <h1 className="Navbar-Name">InkSpace</h1>
+        <ul className="Navbar-Pages">
+            <li><a href="/features">Features</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/pricing">Pricing</a></li>
+            <li><a href="/help">Help</a></li>
+        </ul>
+        <ul className="Navbar-Pages">
+            <li><a href="/login">Login</a></li>
+            <li><a href="/register">Get Started</a></li>
+        </ul>
+    </nav>
     </section>
     <section className="Description">
-        <p>InkSpace is a notebook where you can save or write your notes.<br></br>It's purely built for students,founders and business owners to keep track of their records</p>
+        <p className="rounded-card"><Star/> Your thought. Organized beautifully.</p>
+        <h1 className="heading1">Write. Organize.</h1>
+        <h1 className="heading2">Remember.</h1>
+        <p>InkSpace is a notebook where you can save or write your notes.<br></br>It's purely built for students, founders and business owners<br></br> to keep track of their records</p>
     </section>
     </div>
 );
