b1d37b7b4578321717f732f6681a787b29c0be30
Aryan Sonsurkar <aryansonsurkar87@gmail.com>
2026-08-23 16:41:30 +0530

    Started to Build Landing Page

---
+++ inkspace/app/LandingPage.jsx
@@ -0,0 +1,14 @@
+import "./LandingPage.css"
+
+export default function LandingPage(){
+return(
+    <div>
+    <section className="Heading">
+        <h1>Welcome to InkSpace</h1>
+    </section>
+    <section className="Description">
+        <p>InkSpace is a notebook where you can save or write your notes.<br></br>It's purely built for students,founders and business owners to keep track of their records</p>
+    </section>
+    </div>
+);
+}
