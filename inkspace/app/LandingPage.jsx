a5def2fc4a28d1183e13dbeead65d5dd2074a2d8
Aryan Sonsurkar <aryansonsurkar87@gmail.com>
2026-08-26 22:24:36 +0530

    Got Stuck on the UI

--- inkspace/app/LandingPage.jsx
+++ inkspace/app/LandingPage.jsx
@@ -10,12 +10,20 @@ return(
         <h1 className="Navbar-Name">InkSpace</h1>
         <ul className="Navbar-Pages">
             <li><a href="/features">Features</a></li>
+        </ul>
+        <ul className="Navbar-Pages">
             <li><a href="/about">About</a></li>
+        </ul>
+        <ul className="Navbar-Pages">
             <li><a href="/pricing">Pricing</a></li>
+        </ul>
+        <ul className="Navbar-Pages">
             <li><a href="/help">Help</a></li>
         </ul>
         <ul className="Navbar-Pages">
             <li><a href="/login">Login</a></li>
+        </ul>
+        <ul className="Navbar-Pages">
             <li><a href="/register">Get Started</a></li>
         </ul>
     </nav>
@@ -25,6 +33,10 @@ return(
         <h1 className="heading1">Write. Organize.</h1>
         <h1 className="heading2">Remember.</h1>
         <p>InkSpace is a notebook where you can save or write your notes.<br></br>It's purely built for students, founders and business owners<br></br> to keep track of their records</p>
+    <section className="Buttons">
+        <button><a href="/register">Get Started</a></button>
+        <button><a href="/about">About</a></button>
+    </section>
     </section>
     </div>
 );
